d056c45e55a736993c51eb04b115fb32b7d11f6f
Jean Paul Tuyisenge <jean-paul.tuyisenge@teamflow.de>
2023-12-14 12:23:22 +0200

    Updating callbacks on graphs

--- KafkaVis.py
+++ KafkaVis.py
@@ -15,6 +15,9 @@ UDPs = []
 TCPs = []
 DATA_SIZEs = []
 TIMESTAMPS = []
+UDP_DATA_SIZEs = []
+TCP_DATA_SIZEs = []
+COUNTER = []
 
 app = dash.Dash(__name__)
 print("Dash application initialized...")
@@ -22,29 +25,31 @@ print("Dash application initialized...")
 topic = 'network-events'
 consumer = init_consumer(topic)
 
-app.layout = html.Div(style={'backgroundColor': 'black', 'color': 'white', 'textAlign': 'center', 'margin': '0px'},
+app.layout = html.Div(style={'backgroundColor': 'black', 'color': 'white', 'textAlign': 'center', 'margin': '0px', 'padding': '20px'},
                       children=[
-                          html.H1(children='Network Traffic Analysis'),
+                          html.P(),
+                          html.H1(children='Network Traffic Analysis', style={'marginBottom': '50px'}),
                           html.Div(children=[
                               html.Div(id='live-update-text')]),
                               
                           html.Div(children=[
                             
-                            dcc.Graph(id='live-graph'),
-                            dcc.Graph(id='graph2', style={'marginLeft': '10px'})
-                            ], style={'display': 'flex', 'margin': 'auto', 'justifyContent': 'center', 'marginBottom': '100px'}),
+                            dcc.Graph(id='live-graph_bar'),
+                            dcc.Graph(id='live-graph_line', style={'marginLeft': '10px'})
+                            ], style={'display': 'flex', 'margin': 'auto', 'justifyContent': 'center', 'marginBottom': '150px'}),
+                          
                           html.Div(children=[
-                              
-                              dcc.Interval(id='interval-comp', interval=3000, n_intervals=5),
+                              dcc.Interval(id='interval-comp', interval=3000, n_intervals=0),
+                              dcc.Interval(id='interval-graph', interval=10000, n_intervals=0),
                           ], style={'display': 'flex', 'margin': 'auto', 'justifyContent': 'center'}),
                       ])
 
 @app.callback(Output('live-update-text', 'children'),
               [Input('interval-comp', 'n_intervals')])
 def update_graph(n):
-    global EVENTS, UDPs, TCPs, DATA_SIZEs, TIMESTAMPS
+    global EVENTS, UDPs, TCPs, DATA_SIZEs, TIMESTAMPS, UDP_DATA_SIZEs, TCP_DATA_SIZEs, COUNTER
     msg = consumer.poll(1.0)
-    udp_count, tcp_count, udp_data_size, tcp_data_size = aggreate_data(EVENTS)
+    udp_count, tcp_count, udp_data_size, tcp_data_size, counter = aggreate_data(EVENTS)
     if msg is None:
         print("No message received")
         return html.Div([
@@ -64,49 +69,58 @@ def update_graph(n):
     EVENTS.append(data)
     UDPs.append(udp_count)
     TCPs.append(tcp_count)
-    DATA_SIZEs.append(udp_data_size)
+    DATA_SIZEs.append(udp_data_size + tcp_data_size)
     TIMESTAMPS.append(data['timestamp'])
+    UDP_DATA_SIZEs.append(udp_data_size)
+    TCP_DATA_SIZEs.append(tcp_data_size)
+    COUNTER.append(counter * 3)
 
     return html.Div([
         html.P(f"UDP Count: {udp_count} -- TCP Count: {tcp_count}", style={'color': '#666666'}),
         html.P(f"UDP Data Size: {udp_data_size}  -- TCP Data Size: {tcp_data_size}", style={'color': '#666666'})
     ])
     
-    
 
-@app.callback(Output('live-graph', 'figure'),
-              Output('graph2', 'figure'),
-              [Input('interval-comp', 'n_intervals')])
+@app.callback(Output('live-graph_bar', 'figure'),
+              Output('live-graph_line', 'figure'),
+              [Input('interval-graph', 'n_intervals')])
 def update_graph(n):
-    print("Updating graph...")
-    print("n_intervals: ", n)
-    global UDPs, TCPs, DATA_SIZEs, TIMESTAMPS
+    global UDPs, TCPs, DATA_SIZEs, TIMESTAMPS, UDP_DATA_SIZEs, TCP_DATA_SIZEs, COUNTER
     data = {
+        'Count (Seconds)': COUNTER,
         'timestamp': TIMESTAMPS,
         'UDPs': UDPs,
         'TCPs': TCPs,
+        'DataSize': DATA_SIZEs,
+        'UDPDataSize': UDP_DATA_SIZEs,
+        'TCPDataSize': TCP_DATA_SIZEs
         }
     df = pd.DataFrame(data)
     
-    df["date"] = pd.to_datetime(df["timestamp"])
-    fig = px.line(df, x='TCPs', y="UDPs")
-    
-    fig = px.line(df, x="datetime", y="DataSize", title='Data Size for UDPs and TCPs')
-    fig.add_scatter(x=df["date"], y=df["UDPs"], mode='lines', name='UDP')
-    fig.add_scatter(x=df["date"], y=df["TCPs"], mode='lines', name='TCP')
-    
+    df["DateTime"] = pd.to_datetime(df["timestamp"])
+    print( df["DateTime"])
+    # Create a line chart for life expectancy and GDP per Capita
+    df_last_20 = df.tail(20)
+
+    # Create a line chart for the last 20 data points
+    fig1 = px.line(df_last_20, x="Count (Seconds)", y=["UDPDataSize", "TCPDataSize"], 
+                labels={'value': 'Data Size (KB)', 'variable': 'Metric'})
+    fig1.update_layout(title_text='Data Size for UDPs and TCPs', title_x=0.5)
+
     total_count = len(set(TCPs + UDPs))
-    figure = {
+    fig2 = {
         'data': [
-            {'x':  total_count , 'y': TCPs[-10:], 'type': 'bar', 'name': 'TCPs'},
-            {'x':  total_count , 'y': UDPs[-10:], 'type': 'bar', 'name': 'UPDs'},
+            {'x':  list(range(1, total_count)) , 'y': TCPs[-10:], 'type': 'bar', 'name': 'TCPs'},
+            {'x':  list(range(1, total_count)) , 'y': UDPs[-10:], 'type': 'bar', 'name': 'UPDs'},
         ],
         'layout': {
-            'title': 'Last Ten TCPs and UDPs'
+            'title': {'text': 'TCPs Counts vs UDPs Counts', 'x': 0.5},
+            'xaxis': {'title': 'Index'},
+            'yaxis': {'title': 'No. of IPs'}
         }
     }
     
-    return fig, figure
+    return fig1, fig2
     
 
 # Run the app
--- aggegate.py
+++ aggegate.py
@@ -2,6 +2,7 @@
 def aggreate_data(data):
     """Aggregates the data by protocol and returns the results"""
     # Initialize the variables
+    counter = 0
     udp_count = 0
     tcp_count = 0
     udp_data_size = 0
@@ -11,9 +12,11 @@ def aggreate_data(data):
     for entry in data:
         if entry['protocol'] == 'UDP':
             udp_count += 1
-            udp_data_size += entry['data_size_kb']
+            udp_data_size = entry['data_size_kb']
+            counter += 1
         elif entry['protocol'] == 'TCP':
             tcp_count += 1
-            tcp_data_size += entry['data_size_kb']
+            tcp_data_size = entry['data_size_kb']
+            counter += 1
             
-    return udp_count, tcp_count, udp_data_size, tcp_data_size
+    return udp_count, tcp_count, udp_data_size, tcp_data_size, counter
